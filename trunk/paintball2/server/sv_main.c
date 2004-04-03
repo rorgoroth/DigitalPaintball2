e26f8471209eca62bffc66a46be42cde498de83b
jitspoe <jitspoe@e9e19cf4-851f-4b33-bb0c-e482167b140c>
2004-04-03 09:15:59 +0000

    build 10

--- trunk/paintball2/server/sv_main.c
+++ trunk/paintball2/server/sv_main.c
@@ -969,7 +969,7 @@ void SV_Init (void)
 	Cvar_Get ("cheats", "0", CVAR_SERVERINFO|CVAR_LATCH);
 	Cvar_Get ("protocol", va("%i", PROTOCOL_VERSION), CVAR_SERVERINFO|CVAR_NOSET);;
 	maxclients = Cvar_Get ("maxclients", "1", CVAR_SERVERINFO | CVAR_LATCH);
-	hostname = Cvar_Get ("hostname", "noname", CVAR_SERVERINFO | CVAR_ARCHIVE);
+	hostname = Cvar_Get ("hostname", "Paintball 2.0 (build " BUILD_S ")", CVAR_SERVERINFO | CVAR_ARCHIVE);
 	timeout = Cvar_Get ("timeout", "125", 0);
 	zombietime = Cvar_Get ("zombietime", "2", 0);
 	sv_showclamp = Cvar_Get ("showclamp", "0", 0);
