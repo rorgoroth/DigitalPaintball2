4e47232e2cf9fd87a2ba5bde97d70c498c8fee61
jitspoe <jitspoe@e9e19cf4-851f-4b33-bb0c-e482167b140c>
2004-06-26 03:04:07 +0000

    build 11 - beginning of client side scoreboard

---
+++ paintball2/client/cl_scores.c
@@ -0,0 +1,32 @@
+/*
+Copyright (C) 2003-2004 Nathan Wulf (jitspoe)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+
+
+// === jitmenu
+
+// Client side scoreboard.
+
+char **cl_scores_nums;
+char **cl_scores_info;
+int cl_scores_count;
+
+
+
+// jitmenu ===
