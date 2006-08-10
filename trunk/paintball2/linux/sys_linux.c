8c8b473de8ce4ac849291568a3a9ec29ab4781d5
qudos <qudos@e9e19cf4-851f-4b33-bb0c-e482167b140c>
2006-08-10 18:19:50 +0000

    freebsd compatibility

--- trunk/paintball2/linux/sys_linux.c
+++ trunk/paintball2/linux/sys_linux.c
@@ -35,7 +35,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <sys/wait.h>
 #include <sys/mman.h>
 #include <errno.h>
+#if defined (__linux__)
 #include <mntent.h>
+#endif
 #include <dlfcn.h>
 
 #include "../qcommon/qcommon.h"
