17596dab376d0a953f663d3c42a379c185b80fd1
jitspoe <jitspoe@e9e19cf4-851f-4b33-bb0c-e482167b140c>
2007-05-10 06:23:52 +0000

    build 18 - version number change

--- paintball2/qcommon/qcommon.h
+++ paintball2/qcommon/qcommon.h
@@ -32,14 +32,14 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "md5.h"
 
 #ifdef QUAKE2
-#define VERSION		0.17
+#define VERSION		0.18
 #define BASEDIRNAME "baseq2"
 #define BUILD 0
 #define BUILD_S "0"
 #else
 #define	VERSION		2.0 // jitversion (was 3.21)
-#define BUILD		17 // jitversion / jitbuild -- Paintball2 build number
-#define BUILD_S		"17" // jitversion, for strings.
+#define BUILD		18 // jitversion / jitbuild -- Paintball2 build number
+#define BUILD_S		"18" // jitversion, for strings.
 #define	BASEDIRNAME	"pball" // jit, was "baseq2"
 #endif
 
