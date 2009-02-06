d098ad177411dd9be3aaba8210f7da42b96f715a
viciouz <viciouz@e9e19cf4-851f-4b33-bb0c-e482167b140c>
2009-02-06 14:54:13 +0000

    fix - 64-bit memory bug on linux

--- trunk/paintball2/linux/q_shlinux.c
+++ trunk/paintball2/linux/q_shlinux.c
@@ -17,6 +17,11 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
+
+#if !defined _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <errno.h>
