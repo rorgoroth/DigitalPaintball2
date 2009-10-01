924b14f091a86a372f863f3c067569f6d8aaa10f
rabe85 <rabe85@e9e19cf4-851f-4b33-bb0c-e482167b140c>
2009-10-01 16:53:23 +0000

    Multi-language menus

--- serverbrowser/resource.h
+++ serverbrowser/resource.h
@@ -6,18 +6,15 @@
 #define IDC_STATUSBAR                   3
 #define IDD_SERVERBROWSER_DIALOG        102
 #define IDD_ABOUTBOX                    103
-#define IDS_APP_TITLE1                  104			// String Table - English
-#define IDS_APP_TITLE2                  105			// String Table - German
-#define IDM_ABOUT                       106
-#define IDM_EXIT                        107
-#define IDS_HELLO                       108
-#define IDI_SERVERBROWSER               109
-#define IDI_SMALL                       110
-#define IDC_SERVERBROWSER1              111			// Menu - English
-#define IDC_SERVERBROWSER2              112			// Menu - German
+#define IDS_APP_TITLE                   104
+#define IDM_ABOUT                       105
+#define IDM_EXIT                        106
+#define IDS_HELLO                       107
+#define IDI_SERVERBROWSER               108
+#define IDI_SMALL                       109
+#define IDC_SERVERBROWSER               110
 #define IDR_MAINFRAME                   128
-#define IDR_MENU_TRAY1                  129			// Menu - English
-#define IDR_MENU_TRAY2                  130			// Menu - German
+#define IDR_MENU_TRAY                   129
 #define IDI_DPLOGO						500
 #define IDI_STERN0						501
 #define IDI_STERN1						502
@@ -28,13 +25,11 @@
 #define IDI_WELT2						507
 #define IDM_UPDATE                      32771
 #define IDM_REFRESH                     32772
-#define IDM_LANG_OPTION1                32773
-#define IDM_LANG_OPTION2                32774
-#define ID_TRAY_EXIT                    32775
-#define ID_RIGHTCLICK_COPYADDRESS       32776
-#define ID_RIGHTCLICK_COPYURL           32777
-#define ID_RIGHTCLICK_COPYFULLSERVERINFO 32778
-#define ID_RIGHTCLICK_COPYNAMEIP        32779
+#define ID_TRAY_EXIT                    32773
+#define ID_RIGHTCLICK_COPYADDRESS       32774
+#define ID_RIGHTCLICK_COPYURL           32775
+#define ID_RIGHTCLICK_COPYFULLSERVERINFO 32776
+#define ID_RIGHTCLICK_COPYNAMEIP        32777
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
