46255c52b5c3573eb9dcbf0b2a1dc1e34e9a5b3b
jitspoe <jitspoe@e9e19cf4-851f-4b33-bb0c-e482167b140c>
2009-12-14 18:08:24 +0000

    Merged all resources into one file.  Started work on icons for passwerded servers.

--- serverbrowser/resource.h
+++ serverbrowser/resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by serverbrowser.en-US.rc
+// Used by serverbrowser.rc
 //
 #define IDC_MYICON                      2
 #define IDC_STATUSBAR                   3
@@ -16,14 +16,9 @@
 #define IDM_WEBSITE                     111
 #define IDR_MAINFRAME                   128
 #define IDR_MENU_TRAY                   129
+#define IDI_PASSWORDED                  132
+#define IDI_BLANK                       133
 #define IDI_DPLOGO                      500
-#define IDI_STERN0                      501
-#define IDI_STERN1                      502
-#define IDI_SCHLOSS0                    503
-#define IDI_SCHLOSS1                    504
-#define IDI_WELT0                       505
-#define IDI_WELT1                       506
-#define IDI_WELT2                       507
 #define IDM_UPDATE                      32771
 #define IDM_REFRESH                     32772
 #define ID_TRAY_EXIT                    32773
@@ -31,13 +26,12 @@
 #define ID_RIGHTCLICK_COPYURL           32775
 #define ID_RIGHTCLICK_COPYFULLSERVERINFO 32776
 #define ID_RIGHTCLICK_COPYNAMEIP        32777
-#define IDC_STATIC                      -1
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_RESOURCE_VALUE        135
 #define _APS_NEXT_COMMAND_VALUE         32779
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
