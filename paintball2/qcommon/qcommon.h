fd175a59d00a3a34f1d7d4d5d24c5c018d383f2b
jitspoe <jitspoe@e9e19cf4-851f-4b33-bb0c-e482167b140c>
2010-04-14 05:06:50 +0000

    build 29 - Added DEBUG define for debug builds.

--- paintball2/qcommon/qcommon.h
+++ paintball2/qcommon/qcommon.h
@@ -48,6 +48,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #ifdef NDEBUG
 #define BUILDSTRING "Win32 RELEASE"
 #else
+#define DEBUG
 #define BUILDSTRING "Win32 DEBUG"
 #endif
 
