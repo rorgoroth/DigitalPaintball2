45d8a6360c022a1805604ccfe8974f6354728485
jitspoe <jitspoe@e9e19cf4-851f-4b33-bb0c-e482167b140c>
2010-12-19 09:10:26 +0000

    build 31 - Quick hack to get certain variables from the renderer dll.  Version number change.

--- paintball2/qcommon/qcommon.h
+++ paintball2/qcommon/qcommon.h
@@ -32,14 +32,14 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "md5.h"
 
 #ifdef QUAKE2
-#define VERSION		0.29 // jitversion
+#define VERSION		0.31 // jitversion
 #define BASEDIRNAME "baseq2"
 #define BUILD 0
 #define BUILD_S "0"
 #else
 #define	VERSION		2.0 // jitversion (was 3.21)
-#define BUILD		29 // jitversion / jitbuild -- Paintball2 build number
-#define BUILD_S		"29" // jitversion, for strings.
+#define BUILD		31 // jitversion / jitbuild -- Paintball2 build number
+#define BUILD_S		"31" // jitversion, for strings.
 #define	BASEDIRNAME	"pball" // jit, was "baseq2"
 #endif
 
@@ -940,5 +940,10 @@ void SV_Shutdown (char *finalmsg, qboolean reconnect);
 void SV_Frame (int msec);
 
 
+// jit - little hack to get specific variables from the renderer
+#define VARID_CL_ATTRACTLOOP			1
+#define VARID_G_COMMAND_STUFFED			2
+#define VARID_BUILD						3
+int GetIntVarByID (int id);
 
 
