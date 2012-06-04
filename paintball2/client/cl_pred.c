318fc8dc942eee229528859b41d854861cebac0b
jitspoe <jitspoe@e9e19cf4-851f-4b33-bb0c-e482167b140c>
2012-06-04 00:52:01 +0000

    build 36 - Relocated variable so standalone server would build.

--- paintball2/client/cl_pred.c
+++ paintball2/client/cl_pred.c
@@ -21,7 +21,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "client.h"
 
 
-float g_stepheight = 0.0f; // jitmove - for step smoothing
+extern float g_stepheight; // jitmove - for step smoothing
 
 
 /*
