25bc22116837e4300aba34dfaf1003c5120a90ea
jitspoe <jitspoe@e9e19cf4-851f-4b33-bb0c-e482167b140c>
2012-10-02 16:42:26 +0000

    build 36

--- paintball2/qcommon/qcommon.h
+++ paintball2/qcommon/qcommon.h
@@ -38,10 +38,10 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #define BUILD_S "0"
 #else
 #define	VERSION		2.0 // jitversion (was 3.21)
-#define BUILD		35 // jitversion / jitbuild -- Paintball2 build number
-#define BUILD_S		"35" // jitversion, for strings.
+#define BUILD		36 // jitversion / jitbuild -- Paintball2 build number
+#define BUILD_S		"36" // jitversion, for strings.
 #define BUILD_YEAR	2012 // jitversion
-#define BUILD_MONTH	01 // jitversion
+#define BUILD_MONTH	09 // jitversion
 #define BUILD_DAY	09 // jitversion
 #define	BASEDIRNAME	"pball" // jit, was "baseq2"
 #endif
