8d22d64fa44493253faa1de4bb33a1e7b4e727b7
he3lixxx <he3lixxx@e9e19cf4-851f-4b33-bb0c-e482167b140c>
2015-05-24 22:34:47 +0000

    Support for custom resolutions using "gl_mode -1" and "vid_resx", "vid_resy"

--- trunk/paintball2/win32/vid_dll.c
+++ trunk/paintball2/win32/vid_dll.c
@@ -50,6 +50,8 @@ cvar_t		*vid_xpos;			// X coordinate of window position
 cvar_t		*vid_ypos;			// Y coordinate of window position
 cvar_t		*vid_fullscreen;
 cvar_t		*vid_borderless;	// jitborderless
+cvar_t		*vid_resx;			// xrichardx: custom resulution with gl_mode -1
+cvar_t		*vid_resy;			// xrichardx: custom resulution with gl_mode -1
 
 // Global variables used internally by this module
 viddef_t	viddef;				// global video state; used by other modules
@@ -991,6 +993,17 @@ void VID_CheckChanges (void)
 {
 	char name[100];
 
+	if (vid_resx->modified || vid_resy->modified)
+	{
+		cvar_t *gl_mode = Cvar_Get("gl_mode", "3", CVAR_ARCHIVE);
+		if (gl_mode->value == -1)
+		{
+			vid_ref->modified = true; // force vid_restart
+			vid_resx->modified = false;
+			vid_resy->modified = false;
+		}
+	}
+
 	if (win_noalttab->modified)
 	{
 		if (win_noalttab->value)
@@ -1113,6 +1126,8 @@ VID_Init
 void VID_Init (void)
 {
 	/* Create the video variables so we know how to start the graphics drivers */
+	vid_resx = Cvar_Get("vid_resx", "640", CVAR_ARCHIVE); //xrichardx: custom resolutions with gl_mode -1
+	vid_resy = Cvar_Get("vid_resy", "480", CVAR_ARCHIVE);
 	vid_ref = Cvar_Get("vid_ref", "pbgl", CVAR_ARCHIVE); // jit
 	vid_xpos = Cvar_Get("vid_xpos", "3", CVAR_ARCHIVE);
 	vid_ypos = Cvar_Get("vid_ypos", "22", CVAR_ARCHIVE);
