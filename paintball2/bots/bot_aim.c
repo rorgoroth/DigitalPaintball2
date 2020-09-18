47095958d40c806a039e5d92eb6383e85883272f
jitspoe <jitspoe@e9e19cf4-851f-4b33-bb0c-e482167b140c>
2020-09-18 10:37:35 +0000

    Added latency to bots.  They're now tracking player positions 4 frames (400ms) behind their current position, so they shouldn't be as aimbotty.

--- paintball2/bots/bot_aim.c
+++ paintball2/bots/bot_aim.c
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2015 Nathan "jitspoe" Wulf, Digital Paint
+Copyright (c) 2015-2020 Nathan "jitspoe" Wulf, Digital Paint
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
@@ -24,7 +24,20 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "bot_manager.h"
 #include "bot_debug.h"
 
+
+#define MAX_PLAYER_POS_HISTORY 4
 static const vec3_t vec_zero = { 0.0f, 0.0f, 0.0f };
+static vec3_t player_pos_history[MAX_PLAYER_POS_HISTORY][MAX_CLIENTS];
+static int maxclients = 256;
+
+
+void BotInitAim (void)
+{
+	int size_test2 = sizeof(player_pos_history[0]);
+	memset(player_pos_history, 0, sizeof(player_pos_history));
+	maxclients = (int)(bi.cvar("maxclients", "256", 0)->value);
+}
+
 
 void VecToAngles (vec3_t value1, vec3_t angles)
 {
@@ -124,6 +137,13 @@ void BotAimTowardDesiredAngles (int botindex, int msec)
 	}
 }
 
+// Enforce latency on player positions so the bots don't have inhuman reaction time.
+void GetOldPlayerPosition (const edict_t *ent, vec_t *vec_out)
+{
+	int playerindex = bi.GetPlayerIndexFromEnt(ent);
+	assert(playerindex >= 0 && playerindex < maxclients);
+	VectorCopy(player_pos_history[MAX_PLAYER_POS_HISTORY - 1][playerindex], vec_out);
+}
 
 // Called every frame -- acquire and shoot at targets.
 void BotAimAndShoot (int botindex, int msec)
@@ -139,26 +159,29 @@ void BotAimAndShoot (int botindex, int msec)
 		if (target != self)
 		{
 			vec3_t diff;
+			vec3_t targetpos;
 			float dist_sq;
-
-			VectorSubtract(self->s.origin, target->s.origin, diff);
+			
+			GetOldPlayerPosition(target, targetpos);
+			VectorSubtract(self->s.origin, targetpos, diff);
 			dist_sq = VectorLengthSquared(diff);
 
 			if (dist_sq < best_dist_sq)
 			{
 				if (bi.IsEnemy(self, target))
 				{
-					vec3_t viewpos, targetpos;
+					vec3_t viewpos;
 					trace_t trace;
 
 					// Trace from the bot's eye position to the enemy's position + some randomization (so if only part of the enemy is showing, the bot might see it sometimes, but not all the time).
 					VectorCopy(self->s.origin, viewpos);
 					viewpos[2] += bi.GetViewHeight(self);
-					VectorCopy(target->s.origin, targetpos);
 					targetpos[2] += bi.GetViewHeight(target) + nu_rand(8) - nu_rand(16);
 					targetpos[0] += nu_rand(14) - nu_rand(14);
 					targetpos[1] += nu_rand(14) - nu_rand(14);
 					trace = bi.trace(viewpos, vec_zero, vec_zero, targetpos, self, MASK_SOLID);
+
+					// TODO: Add smoke grenade checks here.
 
 					if (trace.fraction == 1.0f || trace.ent == target)
 					{
@@ -178,12 +201,14 @@ void BotAimAndShoot (int botindex, int msec)
 	if (best_target)
 	{
 		float dist_to_target;
+		vec3_t target_pos;
 		vec3_t to_target;
 		vec3_t best_target_predicted_location;
 		float rand_time, rand_min, rand_max;
 
 		// Distance to target
-		VectorSubtract(self->s.origin, best_target->s.origin, to_target);
+		GetOldPlayerPosition(best_target, target_pos);
+		VectorSubtract(self->s.origin, target_pos, to_target);
 		dist_to_target = VectorLength(to_target);
 		rand_max = 0.2f + dist_to_target / 3000.0f;
 		rand_min = 80.0f / dist_to_target;
@@ -194,7 +219,7 @@ void BotAimAndShoot (int botindex, int msec)
 		if (rand_min > 0.2f || dist_to_target < 0.01f)
 			rand_min = 0.2f;
 
-		rand_time = nu_rand(rand_max) - nu_rand(rand_min); // randomly lead targets a bit, based on distance.
+		rand_time = nu_rand(rand_max) - nu_rand(rand_min) + nu_rand(0.5); // randomly lead targets a bit, based on distance. + compensate for latency.
 		//bi.dprintf("r: %g, min: %g, max: %g\n", rand_time, rand_min, rand_max);
 
 		if (movement->aim_target == best_target)
@@ -203,45 +228,51 @@ void BotAimAndShoot (int botindex, int msec)
 			vec3_t pos_diff;
 			vec3_t predicted_velocity;
 
-			VectorSubtract(best_target->s.origin, movement->last_target_pos, pos_diff);
+			VectorSubtract(target_pos, movement->last_target_pos, pos_diff);
 			VectorScale(pos_diff, 1000.0f / (float)msec, predicted_velocity); // TODO: compensate for bot's own velocity as well.
-			VectorMA(best_target->s.origin, rand_time, predicted_velocity, best_target_predicted_location);
+			VectorMA(target_pos, rand_time, predicted_velocity, best_target_predicted_location);
 		}
 		else
 		{
 			// New target, just aim directly toward it
-			VectorCopy(best_target->s.origin, best_target_predicted_location);
+			VectorCopy(target_pos, best_target_predicted_location);
 			movement->aim_target = best_target;
 		}
 
 		best_target_predicted_location[2] += nu_rand(48.0f) - nu_rand(48.0f); // add some randomization to the verticality to maybe hit crouching/jumping targets by chance.
 		BotSetDesiredAimAnglesFromPoint(botindex, best_target_predicted_location);
 		DrawDebugSphere(best_target_predicted_location, 20.0f, 1.0f, 0.5f, 0.1f, 0.2f, -1);
-		VectorCopy(best_target->s.origin, movement->last_target_pos);
+		VectorCopy(target_pos, movement->last_target_pos);
 		movement->last_target_msec = 0;
 
 		// Shoot if we're aiming close enough to our desired target.
-		if (skill->value > -1)
+		if (movement->shooting_commit_time <= 0.0)
 		{
-			float r;
-			float r_max = 180.0f;
-			float yaw_diff, pitch_diff;
+			if (skill->value > -1)
+			{
+				float r;
+				float r_max = 180.0f;
+				float yaw_diff, pitch_diff;
 			
-			// Find difference between desired aim and current aim.
-			yaw_diff = fabs(AngleTo180(self->s.angles[YAW] - movement->desired_angles[YAW]));
-			pitch_diff = fabs(AngleTo180(self->s.angles[PITCH] * 3.0f - movement->desired_angles[PITCH]));
+				// Find difference between desired aim and current aim.
+				yaw_diff = fabs(AngleTo180(self->s.angles[YAW] - movement->desired_angles[YAW]));
+				pitch_diff = fabs(AngleTo180(self->s.angles[PITCH] * 3.0f - movement->desired_angles[PITCH]));
 
-			// The further away something is, the more precise we want to be.  Somewhat arbitrary -- just tweaked until it felt about right.
-			if (dist_to_target > 32.0f)
-				r_max /= (dist_to_target / 32.0f);
+				// The further away something is, the more precise we want to be.  Somewhat arbitrary -- just tweaked until it felt about right.
+				if (dist_to_target > 32.0f)
+					r_max /= (dist_to_target / 32.0f);
 
-			// Pick a random value within r_max degrees.  If the bot is aiming within that threshold, shoot.
-			r = nu_rand(r_max);
+				// Pick a random value within r_max degrees.  If the bot is aiming within that threshold, shoot.
+				r = nu_rand(r_max);
 
-			if (yaw_diff < r)
-				movement->shooting = true;
+				if (yaw_diff < r)
+				{
+					movement->shooting_commit_time = nu_rand(2.0);
+					movement->shooting = true;
+				}
 
-			//bi.dprintf("%s d: %3.3f, rm: %3.3f, r: %3.3f\n", movement->shooting ? "S" : " ", yaw_diff, r_max, r);
+				//bi.dprintf("%s d: %3.3f, rm: %3.3f, r: %3.3f\n", movement->shooting ? "S" : " ", yaw_diff, r_max, r);
+			}
 		}
 	}
 	else if (movement->aim_target)
@@ -249,7 +280,7 @@ void BotAimAndShoot (int botindex, int msec)
 		movement->last_target_msec += msec;
 
 		// Aim at the last known enemy position for a while, so the bot doesn't instantly turn his back when line of sight is obscurred.
-		if (nu_rand(movement->last_target_msec) < 1000.0f)
+		if (nu_rand(movement->last_target_msec) < 2000.0f)
 		{
 			BotSetDesiredAimAnglesFromPoint(botindex, movement->last_target_pos);
 		}
@@ -259,5 +290,36 @@ void BotAimAndShoot (int botindex, int msec)
 		}
 	}
 
+	movement->shooting_commit_time -= msec * 0.001f;
+
+	if (movement->shooting_commit_time > 0.0)
+	{
+		movement->shooting = true;
+	}
+
 	BotAimTowardDesiredAngles(botindex, msec);
+}
+
+// Called every server frame (.1s)
+void UpdatePlayerPosHistory (int msec)
+{
+	int clientindex;
+	int historycopyindex;
+
+	// shift everything over
+	for (historycopyindex = MAX_PLAYER_POS_HISTORY - 1; historycopyindex > 0; --historycopyindex)
+	{
+		memcpy(player_pos_history[historycopyindex], player_pos_history[historycopyindex - 1], sizeof(vec3_t) * maxclients);
+	}
+
+	// Put the new
+	for (clientindex = 0; clientindex < maxclients; ++clientindex)
+	{
+		edict_t *ent = bi.GetPlayerEntity(clientindex);
+
+		if (ent && bi.CanInteract(ent))
+		{
+			VectorCopy(ent->s.origin, player_pos_history[0][clientindex]);
+		}
+	}
 }
